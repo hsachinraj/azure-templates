a62310932ec6c8e8a720268647f4aa4a6a2e329a
Sachin Raj <h.sachin@outlook.com>
2016-02-16 15:26:30 -0800

    Update java-tomcat-mysql-install.sh

--- openjdk-tomcat-mysql-ubuntu-vm/java-tomcat-mysql-install.sh
+++ openjdk-tomcat-mysql-ubuntu-vm/java-tomcat-mysql-install.sh
@@ -7,13 +7,25 @@ sudo apt-get -y update --fix-missing
 sudo apt-get install -y $1
 
 # Install tomcat
-sudo apt-get install -y  $2
+#sudo apt-get install -y  $2
+
+
+#edit the tomcat users file
+sudo apt-get install -y tomcat7 tomcat7-admin
+sed -i "s#</tomcat-users>##g" /etc/tomcat7/tomcat-users.xml; \
+	echo '  <role rolename="manager-gui"/>' >>  /etc/tomcat7/tomcat-users.xml; \
+	echo '  <role rolename="manager-script"/>' >>  /etc/tomcat7/tomcat-users.xml; \
+	echo '  <role rolename="manager-jmx"/>' >>  /etc/tomcat7/tomcat-users.xml; \
+	echo '  <role rolename="manager-status"/>' >>  /etc/tomcat7/tomcat-users.xml; \
+	echo '  <role rolename="admin-gui"/>' >>  /etc/tomcat7/tomcat-users.xml; \
+	echo '  <role rolename="admin-script"/>' >>  /etc/tomcat7/tomcat-users.xml; \
+	echo '  <user username="admin" password="P2ssw0rd" roles="manager-gui, manager-script, manager-jmx, manager-status, admin-gui, admin-script"/>' >>  /etc/tomcat7/tomcat-users.xml; \
+	echo '</tomcat-users>' >> /etc/tomcat7/tomcat-users.xml
 
 if netstat -tulpen | grep 8080
 then
 	exit 0
 fi
-
 #Install MySQL
 mysqlversion=$3
 mysqlPassword='password'
