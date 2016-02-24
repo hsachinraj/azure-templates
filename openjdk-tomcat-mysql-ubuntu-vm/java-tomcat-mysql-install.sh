4494dcfe2edaefb82aad163cb09d5f49e828d633
Sachin Raj <h.sachin@outlook.com>
2016-02-24 14:03:02 -0800

    Update java-tomcat-mysql-install.sh

--- openjdk-tomcat-mysql-ubuntu-vm/java-tomcat-mysql-install.sh
+++ openjdk-tomcat-mysql-ubuntu-vm/java-tomcat-mysql-install.sh
@@ -44,6 +44,6 @@ sudo apt-get -y install mysql-server
 
 #fi
 
-mysql -u root --password=$mysqlPassword -e 'create database alm'
-mysql -u root --password=$mysqlpassword < mydbscript.script
+mysql -u root --password="$mysqlPassword" -e 'create database alm'
+mysql -u root --password="$mysqlpassword" < mydbscript.script
 
