59562c59d3032e67f7f4304df6fcf8d7b99f6630
Sachin Raj <h.sachin@outlook.com>
2016-02-24 10:46:34 -0800

    Update java-tomcat-mysql-install.sh

--- openjdk-tomcat-mysql-ubuntu-vm/java-tomcat-mysql-install.sh
+++ openjdk-tomcat-mysql-ubuntu-vm/java-tomcat-mysql-install.sh
@@ -20,27 +20,3 @@ sed -i "s#</tomcat-users>##g" /etc/tomcat7/tomcat-users.xml; \
 	echo '</tomcat-users>' >> /etc/tomcat7/tomcat-users.xml
 
 
-#Install MySQL
-mysqlPassword=$4
-sudo apt-get update
-#no password prompt while installing mysql server
-export DEBIAN_FRONTEND=noninteractive
-
-#another way of installing mysql server in a Non-Interactive mode
-echo "mysql-server mysql-server/root_password select $mysqlPassword" | sudo debconf-set-selections 
-echo "mysql-server mysql-server/root_password_again select $mysqlPassword" | sudo debconf-set-selections 
-#download the script file to be executed
-sudo wget https://raw.githubusercontent.com/hsachinraj/azure-templates/master/openjdk-tomcat-mysql-ubuntu-vm/mydbscript.script
-echo "file downloaded"
-
-#install mysql if not installed before
- status = $(dpkg-query -W -f='${Status}' mysql-server | awk '{print $3}')
- if [ "$status" = "installed" ];
- then
-#install mysql-server 
-echo "Skipping installation as package is alreayd installed"
-sudo apt-get -y install mysql-server
-fi
-
-mysql -u root --password=$mysqlPassword -e 'create database alm'
-mysql -u root --password=$mysqlpassword -D alm < mydbscript.script
